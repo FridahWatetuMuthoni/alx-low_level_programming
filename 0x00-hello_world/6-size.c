2c9a50c9cd5163ff765eac9516856f355af97a59
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-16 09:49:06 -0700

    sizes of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main = The main function prints the various sizes of datatypes in c
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	printf("Size of an char: %lu byte(s)", sizeof(char);
+	printf("Size of an int: %lu bytes(s)", sizeof(int);
+	printf("Size of a long int: %lu bytes(s)", sizeof(long int);
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int);
+	printf("Size of a float: %lu byte(s)", sizeof(float);
+	return (0);
+}
