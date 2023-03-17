bc119d8b6477bebb94fe9e4a9899aa4fbb7f2c28
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-17 08:33:05 -0700

    writes numbers one to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints number one to nine
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	return (0);
+}
