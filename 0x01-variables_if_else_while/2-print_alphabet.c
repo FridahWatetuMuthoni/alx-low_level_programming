8732ba34e7d1ad1f235c9289f2be3c8074168679
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-17 06:57:04 -0700

    prints alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - The main function prints out the alphabets
+ *
+ * Return: always returns zero
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 97;
+	while (a >= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar("/n");
+	return (0);
+}
+
