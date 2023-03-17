c9c78752d5ff4ff98b8720821dedc57d4581700d
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-17 13:30:04 -0700

    all posible number combination between 1-9

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 98; i++)
-		for (j = 0; j <= 99; j++)
+	for (i = 0; i <= 99; i++)
+		for (j = i; j <= 99; j++)
 		{
 			putchar('0' + i / 10);
 			putchar('0' + i % 10);
 			putchar(' ');
 			putchar('0' + j / 10);
 			putchar('0' + j % 10);
-			if (i % 10 != 8 || j % 10 != 9)
+			if (i != 99 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
