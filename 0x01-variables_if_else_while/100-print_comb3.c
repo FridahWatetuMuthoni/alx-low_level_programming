edd5f61f0669dcf90c61b8fddf26ae36712da3ed
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-17 10:51:15 -0700

    least possible combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 
 	for (i = 0; i <= 8; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= i9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
 			if (!(i == 8 || j == 9))
 			{
 				putchar(',');
+				putchar(' ');
 			}
-			putchar(' ');
 		}
 	}
 	return (0);
