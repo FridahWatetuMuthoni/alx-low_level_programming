c77074de89e585fa2806fae8a0e637e19924be72
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-21 14:39:22 -0700

    fibnocci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - first 50 Fibonacci numbers
+ *
+ * Return: always returns  0.
+ */
+
+0int main(void)
+{
+	int i, first = 0, second = 1, total;
+
+	for (i = 0; i < 50; i++)
+	{
+		total = first + second;
+		printf("%lu", total);
+
+		first = second;
+		second = total;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
