a222844cf40519287dc48fea15ea892773e27ecc
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-27 10:54:08 -0700

    random password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,45 +1,53 @@
-#include "main.h"
-#include <time.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
- * password_generator - generates a random password
+ * main - Generates random valid passwords
  *
- * Return: does not return anything
+ * Return: Always 0.
  */
-
-void password_generator(void)
+int main(void)
 {
-	int password[100];
-	int i, sum , num;
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	sum = 0;
-	srand(time(NULL));
+	srand(time(0));
 
-	for (i = 0; i < 100; i++)
+	while (sum < 2772)
 	{
-		password[i] = rand() % 78;
-		sum += (password[i] + '0');
-		putchar(password[i] + '0');
-		if((2772 - sum) - ('0' < 78))
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
 		{
-			num = 2772 - sum - '0';
-			sum += num;
-			putchar(num + '0');
-			break;
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
 		}
 	}
 
-}
+	printf("%s", password);
 
-/**
- * main - calls the password generator function
- *
- * Return: always returns 0
- */
-
-int main(void)
-{
-	password_generator();
 	return (0);
 }
