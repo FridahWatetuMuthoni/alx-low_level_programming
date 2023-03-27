19de29d6aa2ded59496b53325856406b436f21e3
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-27 03:21:39 -0700

    printing a reserved string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev- reserves and prints th reversed string
+ * @s: the string to be reversed
+ *
+ * Return: returns nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
