f1e8c86c4566d1d8bdff0a1178b6fdf6737a2a43
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-29 09:40:35 -0700

    leet -replaces letters with numerics

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,28 +10,18 @@
 char *leet(char *str)
 {
 	int i = 0;
+	int j;
+
+	char letters[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; j <= 7; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == letters[j] || str[i] - 32 == letters[j])
+			{
+				str[i] = j + '0';
+			}
 		}
 		i++;
 	}
