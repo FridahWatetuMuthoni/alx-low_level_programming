75522543efd75cbddaff9d0e414a2c2dfbcad546
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-29 07:07:58 -0700

    a function that capitalizes a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - the function capitalizes the string
+ * @str: the string being capitalized
+ *
+ * Return: resturns a pointer to a string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n')
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - ('a' - 'A');
+			}
+		}
+		i++;
+	}
+	return (str);
+}
