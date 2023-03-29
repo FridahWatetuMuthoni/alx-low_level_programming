9b67db0965efa7bb21db04277f273f26c2652f6f
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-29 05:23:10 -0700

    a string that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcmp -  compares if two strings are equal or the same
+ * @s1: the first string being compared
+ * @s2: the second string being compared
+ *
+ * Return: returns 1 if the strings are the same and
+ * returns 0 if the strings and not
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int s1_len = 0;
+	int s2_len = 0;
+
+	while (s1[s1_len] != '\0')
+	{
+		s1_len++;
+	}
+	while (s2[s2_len] != '\0')
+	{
+		s2_len++;
+	}
+
+	for (i = 0; i < s1_len && i < s2_len; i++)
+	{
+		if (s1[i] == s2[i])
+			return (0);
+		else
+			return (s1[i] - s2[i]);
+	}
+
+	return (s1_len > s2_len ? 1 : -1);
+}
