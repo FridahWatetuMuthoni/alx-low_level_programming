ec68d2765cc145117cf3cecda6b8058afdea6e06
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-03-29 08:41:51 -0700

    comparing two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,25 +11,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int s1_len = 0;
-	int s2_len = 0;
-
-	while (s1[s1_len] != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		s1_len++;
-	}
-	while (s2[s2_len] != '\0')
-	{
-		s2_len++;
-	}
-
-	for (i = 0; i < s1_len && i < s2_len; i++)
-	{
-		if (s1[i] == s2[i])
-			return (0);
-		else
-			return (s1[i] - s2[i]);
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 }
