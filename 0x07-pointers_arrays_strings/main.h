b3a08e88715c74bb464ba4d4dc36e1556f01ea5e
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-03 14:49:43 -0700

    the sum of diagonals in a matrix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
@@ -16,5 +18,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
 
 #endif
