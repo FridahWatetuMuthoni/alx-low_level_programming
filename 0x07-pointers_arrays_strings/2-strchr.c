f09e7009d832c5ee33a4941164a370eab1f5d65c
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-03 11:27:20 -0700

    finds the first occurence of a charater

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,7 +10,12 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s >= '\0')
+	if (*s == '\0')
+	{
+		return ('\0');
+	}
+
+	while (*s != '\0')
 	{
 		if (*s == c)
 		{
@@ -18,5 +23,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return (0);
+	return ('\0');
 }
