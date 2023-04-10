6905453c92524c480e55e0777120458abd031088
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-10 09:29:23 -0700

    fumction adds the commandline arguements

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main-adds the commandline arguements that are postive
+ * @argc: the number of commandline arguements
+ * @argv: the array of commandline arguements
+ *
+ * Return: returns 0 is arguements are passed else returns 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i][0]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
