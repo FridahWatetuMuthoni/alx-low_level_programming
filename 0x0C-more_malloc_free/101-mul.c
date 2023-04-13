f438f823b54272327f0435f6bd15b0a5b09f13fd
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-13 12:46:17 -0700

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -35,12 +35,23 @@ int _strlen(char *str)
 	{
 		len++;
 	}
-
-	return(len);
+	return (len);
 }
 
 /**
- * main- accepts two values from the commandline and 
+ * print_error - prints error if any
+ *
+ * Return: returns nothing
+ */
+
+void print_error(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main- accepts two values from the commandline and
  * performs multiplication
  * @argc: the number of commandline arguements
  * @argv: the array that contains the commandline arguements
@@ -51,67 +62,51 @@ int _strlen(char *str)
 
 int main(int argc, char *argv[])
 {
-	char *num1;
-	char *num2;
+	char *num1, *num2;
 	int *result;
-	int digit1, digit2, sum;
-	int num1_len, num2_len, i, j, carry, len;
+	int digit1, digit2, num1_len, num2_len, i, j, carry, len, sum = 0;
 
 	if (argc != 3 || !is_valid(argv[1]) || !is_valid(argv[2]))
 	{
-		printf("Error\n");
-		exit(98);
+		print_error();
 	}
-
-	num1 = argv[1];
-	num2 = argv[2];
+	num1 = argv[1], num2 = argv[2];
 	num1_len = _strlen(num1);
 	num2_len = _strlen(num2);
 	len = num1_len + num2_len;
-
 	result = malloc(sizeof(int) * len);
-
 	if (result == NULL)
 	{
-		printf("Error\n");
-		exit(98);
+		return (1);
 	}
-
 	for (i = 0; i < len; i++)
 	{
 		result[i] = 0;
 	}
-
 	for (i = num1_len - 1; i >= 0; i--)
 	{
+		digit1 = num1[i] - '0';
 		carry = 0;
 		for (j = num2_len - 1; j >= 0; j--)
 		{
-			digit1 = num1[i] - '0';
 			digit2 = num2[j] - '0';
-			sum = digit1 * digit2 + result[i + j + 1] + carry;
-			result[i + j + 1] = sum % 10;
-			carry = sum / 10;
+			carry += (digit1 * digit2) + result[i + j + 1];
+			result[i + j + 1] = carry % 10;
+			carry /= 10;
 		}
-		result[i + j + 1] = carry;
+		if (carry > 0)
+			result[i + j + 1] += carry;
 	}
-
-	i = 0;
-	while (i < len && result[i] == 0)
+	for (i = 0; i < len - 1; i++)
 	{
-		i++;
+		if (result[i])
+			sum = 1;
+		if (sum)
+			_putchar(result[i] + '0');
 	}
-	if (i == len)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (; i < len; i++)
-	{
-		printf("%d", result[i]);
-	}
-	printf("\n");
+	if (!sum)
+		_putchar('0');
+	_putchar('\n');
 	free(result);
 	return (0);
 }
