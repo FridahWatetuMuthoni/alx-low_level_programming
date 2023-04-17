bb41e902e1a87b85c519ce506fa19270488c05e5
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-17 07:03:09 -0700

    creating a dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - creates the dog's information
+ * @name: name of the dog
+ * @age:age of the dog
+ * @owner: owner of the dog
+ *
+ * Description:the struct creates the info on the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
