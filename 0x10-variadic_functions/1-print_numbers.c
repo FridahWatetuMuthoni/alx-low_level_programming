fcee94b15dbc5be8efc3ecdc29cd69735cd74230
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-20 09:47:24 -0700

    a function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -28,11 +28,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%d%s", num, separator);
+			printf("%d", num);
+			printf("%s", separator);
 		}
 	}
 
-	va_end(args);
-
 	printf("\n");
+	va_end(args);
 }
