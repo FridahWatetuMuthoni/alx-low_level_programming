a50eb4652f0da29a3d18838815763b781edb7453
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-20 09:39:06 -0700

    a function that prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers-the function prints numbers that are seperated by a seperator
+ * @separator: what is being used to seperate the numbers
+ * @n: the number of arguements passed
+ *
+ * Return: the function does not return anything
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		int num = va_arg(args, int);
+
+		if (separator == NULL || i == (n - 1))
+		{
+			printf("%d", num);
+		}
+		else
+		{
+			printf("%d%s", num, separator);
+		}
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
