ac616b38cebd49ec52582c2d4539b03b67cbae75
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-27 12:49:48 -0700

    printing a singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,22 +9,22 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes;
+	size_t len;
 
-	nodes = 0;
-	while (h)
+	len = 0;
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nill)\n");
+			printf("[0](nill)\n");
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
 
-		nodes++;
+		len++;
 		h = h->next;
 	}
-	return (nodes);
+	return (len);
 }
