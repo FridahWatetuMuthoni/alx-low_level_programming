a47183d568e0e05203219a70044e6a39b15475ec
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-27 14:15:30 -0700

    feeing memory in a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list-frees the memory created when creating a new node
+ * @head: pointer to the head of the linked list
+ *
+ * Return: does not return anything
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
