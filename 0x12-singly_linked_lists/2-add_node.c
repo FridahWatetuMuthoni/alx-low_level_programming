6c84990d83c4bbb5e33b62090258b5a5656de34b
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-27 13:40:05 -0700

    adding a node to a linked list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,56 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * _strlen-claculates the length of a string
+ * @str: the string
+ * Return: returns the length of the string
+ */
+
+int _strlen(const char *str)
+{
+	int len, i;
+
+	len = 0;
+	for (i = 0; str[i]; i++)
+		len++;
+	return (len);
+}
+
+/**
+ * add_node-adding a node at the beginning of a linked list
+ * @head: the pointer to the head of the list_t list
+ * @str: the string that we are adding
+ *
+ * Return: returns the address of the element or null
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new_node;
+	char *string_dup;
+	int len;
+
+	new_node = (list_t *)malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	string_dup = strdup(str);
+	if (string_dup == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	len = _strlen(str);
+
+	new_node->str = string_dup;
+	new_node->len = len;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (new_node);
+}
