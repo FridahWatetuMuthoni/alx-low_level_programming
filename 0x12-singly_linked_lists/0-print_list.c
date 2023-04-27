06738564e9327b12c17cccad41266e8cb8ec7a7b
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-27 12:42:26 -0700

    printing a singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 	size_t nodes;
 
 	nodes = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
@@ -23,8 +23,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 		}
 
-		h = h->next;
 		nodes++;
+		h = h->next;
 	}
 	return (nodes);
 }
